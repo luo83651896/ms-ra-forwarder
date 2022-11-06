23f943e9f28602fc8a765c19ddec0cdb9be3f337
ll <luo@test.com>
2022-11-06 20:44:09 +0800

    update

--- api/ra.ts
+++ api/ra.ts
@@ -22,8 +22,8 @@ module.exports = async (request: Request, response: Response) => {
     if (!FORMAT_CONTENT_TYPE.has(format)) {
       throw `无效的音频格式：${format}`
     }
-
-    let ssml = request.body
+    let _body=request.body;
+    let ssml = ssml =`<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US"><voice name=${_body.name||"zh-CN-YunxiNeural"}><prosody>${_body.text}</prosody></voice ></speak >`;
     if (ssml == null) {
       throw `转换参数无效`
     }
